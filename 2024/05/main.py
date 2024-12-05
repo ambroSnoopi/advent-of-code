840fbb97f9165e05e7a4039e0902d99c950ff73a
ambroSnoopi <ambrosini_marco@hotmail.com>
2024-12-05 23:07:25 +0100

    05: part 2 solved

--- 2024/05/main.py
+++ 2024/05/main.py
@@ -18,6 +18,7 @@ with open("input/ordering_rules.csv", "r") as file:
 print(before_than)
 print(later_than)
 
+# %%
 def validate_page_order(update: list[int]) -> bool:
     isInOrder = True
     for index, page in enumerate(update):
@@ -27,6 +28,43 @@ def validate_page_order(update: list[int]) -> bool:
         isInOrder = isInOrder and after.isdisjoint(before_than.get(page)) and prior.isdisjoint(later_than.get(page))
         if not isInOrder: break
     return isInOrder
+
+def order_pages(update: list[int], debug=False) -> list[int]:
+    if debug: print(f"Original update: {update}")
+    ordered_update: list[int] = []
+    for page in update:
+        if not ordered_update:
+            ordered_update.append(page)
+            continue #i.e. skip first iter
+
+        inserted = False
+        for i, p in enumerate(ordered_update):
+            if page in before_than.get(p): 
+                ordered_update.insert(i, page)
+                inserted = True
+                if debug: 
+                    print(f"Page {page} was inserted before {p}.")
+                    print(f"Ordered update: {ordered_update}")
+                break
+        if inserted: continue
+
+        ordered_update.reverse()
+        for i, p in enumerate(ordered_update):
+            if page in later_than.get(p): 
+                ordered_update.insert(i, page)
+                inserted = True
+                if debug: print(f"Page {page} was inserted after {p}.")
+                break
+        ordered_update.reverse()
+        
+        if not inserted:
+            ordered_update.append(page) # if it wasn't in either rule set, we should be able to just append it, right?
+            if debug: print(f"Page {page} was appended.")
+        if debug: print(f"Ordered update: {ordered_update}")
+    assert validate_page_order(ordered_update), f"Ordering failed!\n  Original update: {update}\n  Disordered update: {ordered_update}"
+
+    return ordered_update
+
 # %%
 updates = {
     "toproc": [],
@@ -38,12 +76,17 @@ with open("input/updates.csv", "r") as file:
         update = list(map(int, line.split(',')))
         updates["toproc"].append(update)
 
-middle_pages: list[int] = []
+ordered_middle_pages: list[int] = []
+disordered_middle_pages: list[int] = []
 for update in updates["toproc"]:
     isInOrder = validate_page_order(update)
     if isInOrder:
         updates["ordered"].append(update)
-        middle_pages.append(update[int((len(update)-1)/2)])
-    #else: updates["disordered"].append(update) #if we need for part2...
+        ordered_middle_pages.append(update[int((len(update)-1)/2)])
+    else: # i knew it! :D
+        ordered_update = order_pages(update)
+        updates["disordered"].append(ordered_update)
+        disordered_middle_pages.append(ordered_update[int((len(update)-1)/2)])
 
-print(sum(middle_pages))
+print(sum(disordered_middle_pages))
+# %%
