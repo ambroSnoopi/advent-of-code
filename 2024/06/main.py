7fb3da6323b36f442ff5becd3a4c7a9efc1266c8
ambroSnoopi <ambrosini_marco@hotmail.com>
2024-12-06 11:41:50 +0100

    06: minor pretification (part 1)

--- 2024/06/main.py
+++ 2024/06/main.py
@@ -1,16 +1,14 @@
 import map as m
-# Load the map
+
 map = m.load_map('input.txt')
 
-# Print initial state
 print("Initial state:")
 print(map)
 
-# Simulate guard movement until they leave the map
-path = map.simulate_guard_movement(0)  # Simulate first guard
+path = map.simulate_guard_movement(0)  # Simulate (first) guard movement until it leaves the map
 
 print("\nAfter guard movement:")
 print(map)
 
-print("Guard's path:", len(path))
+print("\nGuard's path:", len(path))
 print("Guard's distinct path:", len(set(path)))
