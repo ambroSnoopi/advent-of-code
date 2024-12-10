ed7056ca46a0ef37b4e92eee7c8dbf2ae4a2a3d1
ambroSnoopi <ambrosini_marco@hotmail.com>
2024-12-10 21:03:28 +0100

    boilerplate

--- 2024/dd/main.py
+++ 2024/dd/main.py
@@ -5,13 +5,13 @@ test.do()
 expected = 0123
 actual = test.checksum()
 assert expected==actual, f"Test failed!\n  Expected: {expected}\n  Actual: {actual}"
-
+"""
 test2 = puzzle.load_puzzle('input/test.txt')
 test2.do()
 expected = 0123
 actual = test2.checksum()
 assert expected==actual, f"Test2 failed!\n  Expected: {expected}\n  Actual: {actual}"
-
+"""
 quest = puzzle.load_puzzle('input/quest.txt')
 quest.do()
 actual = quest.checksum()
