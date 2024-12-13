c49feef7e82b391ef1a906f229ed4e5a5508118c
ambroSnoopi <ambrosini_marco@hotmail.com>
2024-12-13 01:40:03 +0100

    12: shortcuts are evil

--- 2024/12/puzzle.py
+++ 2024/12/puzzle.py
@@ -21,26 +21,38 @@ class Region:
     def __init__(self, cell: Cell):
         self.plant = cell.plant
         self.plots: dict[Tuple[int,int], Cell] = {(cell.x, cell.y): cell}
-        self.max_x = cell.x
-        self.max_y = cell.y
         self.area = 1
         self.perimeter = 4 #TODO: What's the best way of deriving this / storing data for this?
 
+    @classmethod
+    def from_regions(cls, regions: list["Region"]) -> "Region":
+        first_cell = next(iter(regions[0].plots.values()))
+        new_region = cls(first_cell)
+
+        merged_cells = set()
+        for region in regions:
+            merged_cells.update(region.plots.values())
+        for c in merged_cells:
+            if c != first_cell:
+                new_region.add_plot(c)
+        return new_region
+    
     def add_plot(self, cell: Cell):
         """Adds the Cell to the Plots of this Region.""" #, if it contains the same Plant
         #if cell.plant == self.plant:
         self.plots[(cell.x, cell.y)] = cell
-        self.max_x = max(cell.x, self.max_x)
-        self.max_y = max(cell.y, self.max_y)
         self.area += 1
-        n = self.count_prev_neighbors(cell)
-        if n < 2:
-            self.perimeter += 2
+        n = self.count_n(cell)
+        self.perimeter += 4-(2*n)
         return
 
     def count_prev_neighbors(self, cell: Cell) -> int:
         """Counts the number of *previous* neighbors in the Region."""
         return sum(1 for x, y in [(cell.x - 1, cell.y), (cell.x, cell.y - 1)] if self.plots.get((x, y)))
+    
+    def count_n(self, cell: Cell) -> int:
+        """Counts the number of neighbors in the Region."""
+        return sum(1 for x, y in [(cell.x - 1, cell.y), (cell.x, cell.y - 1), (cell.x + 1, cell.y), (cell.x, cell.y + 1)] if self.plots.get((x, y)))
     
     def price(self) -> int:
         return self.area * self.perimeter
@@ -51,7 +63,7 @@ class Map:
         self.height = len(lines)
         self.width = len(lines[0]) if self.height > 0 else 0
         self.cells = np.full((self.height, self.width), "", dtype=Cell)
-        self.regions: dict[Tuple[int,int,str], Region] = {} #(max_x,max_y,plant):Region
+        self.regions: dict[str, list[Region]] = {} #by plant
 
         for y, line in enumerate(lines):
             for x, char in enumerate(line):
@@ -59,19 +71,19 @@ class Map:
                 c = Cell(x, y, plant)
                 self.cells[y][x] = c
 
-                #check left and top neighbor if they can form a region, or create a new one otherwise
-                r = self.regions.pop((x-1, y, plant), None)
-                if r:
-                    r.add_plot(c)
-                    self.regions[(x, y, plant)] = r
+                regions = self.regions.pop(plant, [Region(c)])
+                n_regions = [r for r in regions if r.count_prev_neighbors(c) > 0]
+                if len(n_regions)>1:
+                    merged_region = Region.from_regions(n_regions)
+                    merged_region.add_plot(c)
+                    regions.append(merged_region)
+                    for r in n_regions:
+                        regions.remove(r)
+                elif len(n_regions)==1:
+                    n_regions[0].add_plot(c)
                 else:
-                    r = self.regions.pop((x, y-1, plant), None)
-                    if r:
-                        r.add_plot(c)
-                        self.regions[(x, y, plant)] = r
-                    else:
-                        self.regions[(x, y, plant)] = Region(c)
-
+                    regions.append(Region(c))
+                self.regions[plant] = regions
 
     def get_cells(self) -> Iterator[Cell]:
         """Returns an iterator over all cells in the map, row by row."""
@@ -79,7 +91,8 @@ class Map:
             yield from row
 
     def total_price(self) -> int:
-        return sum(r.price() for r in self.regions.values())
+        return sum(r.price() for region_list in self.regions.values() for r in region_list)
+
 
 def load_puzzle(filename: str):
     with open(filename, 'r') as file:
