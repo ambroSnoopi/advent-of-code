6cb132b88a41907e7b23247d4b6c99585ab8331a
MAmbrosini <ambrosini_marco@hotmail.com>
2024-12-20 23:36:26 +0100

    20: part 2 solved so, their example was indeed wrong and i have been waisting time for nothing... there is only one cheat that gives 76 advantage

--- 2024/20/main.py
+++ 2024/20/main.py
@@ -10,15 +10,16 @@ if __name__ == '__main__':
     test.find_cheats(74, 6)
     expected = 7
     actual = test.checksum()
-    #assert expected==actual, f"Test2 failed!\n  Expected: {expected}\n  Actual: {actual}" # wrong example?!
+    #assert expected==actual, f"Test2 failed!\n  Expected: {expected}\n  Actual: {actual}" 
+    # wrong example! there is only 1 cheat that gives 76 advantage, not 3!
 
     quest = puzzle.load_puzzle('input/quest.txt')
-    #quest.find_cheats(100)
-    #actual = quest.checksum()
-    #print("Quest Checksum:", actual)
-    #
-    #expected = 1402
-    #assert expected==actual, f"Part 1 failed!\n  Expected: {expected}\n  Actual: {actual}"
+    quest.find_cheats(100)
+    actual = quest.checksum()
+    print("Quest Checksum:", actual)
+    
+    expected = 1402
+    assert expected==actual, f"Part 1 failed!\n  Expected: {expected}\n  Actual: {actual}"
 
     quest.find_cheats(100, 20)
     actual = quest.checksum()
