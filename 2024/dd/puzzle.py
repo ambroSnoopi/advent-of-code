d431db5fe67aecbbcc7fafedf41080d2f24843f2
ambroSnoopi <ambrosini_marco@hotmail.com>
2024-12-20 09:04:12 +0100

    expand boilerplate with Puzzle "interface"

---
+++ 2024/dd/puzzle.py
@@ -0,0 +1,22 @@
+from tqdm import tqdm
+
+class Puzzle:
+    def __init__(self, data):
+        pass
+
+    def do(self):
+        pass
+
+    def checksum(self):
+        pass
+    
+    def check_p2(self):
+        pass
+
+def load_puzzle(input_file: str) -> Puzzle:
+    """Load Puzzle from input file (path)."""
+    with open(input_file, 'r') as file:
+        content = file.read()
+        lines = content.strip().split('\n')
+        data = [x for x in lines]
+    return Puzzle(data)
