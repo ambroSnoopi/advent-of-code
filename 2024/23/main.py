9d71b23985a0883e7230c714d0fc7ef249127bfc
MAmbrosini <ambrosini_marco@hotmail.com>
2025-01-03 19:31:57 +0100

    23: part 2 first attempt so maybe we can ignore start with "t" now?

--- 2024/23/main.py
+++ 2024/23/main.py
@@ -1,35 +1,29 @@
 import puzzle
 
 test = puzzle.load_puzzle('input/test.txt')
-test.do()
+test.do(max_size=3)
 expected = 12
 actual = test.checksum()
 assert expected==actual, f"Test failed!\n  Expected: {expected}\n  Actual: {actual}"
 
+test.do(max_size=3, starts_with="t")
 expected = 7
-actual = test.checksum(starts_with="t")
+actual = test.checksum()
 assert expected==actual, f"Test failed!\n  Expected: {expected}\n  Actual: {actual}"
-"""
-test2 = puzzle.load_puzzle('input/test.txt')
-test2.do()
-expected = 0123
-actual = test2.checksum()
-assert expected==actual, f"Test2 failed!\n  Expected: {expected}\n  Actual: {actual}"
 
-#or
-
-expected = 01234
+test.do(starts_with="t")
+expected = 'co,de,ka,ta'
 actual = test.check_p2()
-assert expected==actual, f"Test2 failed!\n  Expected: {expected}\n  Actual: {actual}"
-"""
+assert expected==actual, f"Test failed!\n  Expected: {expected}\n  Actual: {actual}"
+
 quest = puzzle.load_puzzle('input/quest.txt')
-quest.do()
-actual = quest.checksum(starts_with="t")
+quest.do(max_size=3, starts_with="t")
+actual = quest.checksum()
 print("Quest Checksum:", actual)
-"""
-expected = 0123
+
+expected = 1119
 assert expected==actual, f"Part 1 failed!\n  Expected: {expected}\n  Actual: {actual}"
 
+quest.do(starts_with="t")
 p2 = quest.check_p2()
 print("Quest Checksum Part 2:", p2)
-"""
--- 2024/23/puzzle.py
+++ 2024/23/puzzle.py
@@ -9,36 +9,38 @@ class Puzzle:
             pc1, pc2 = connection.split('-')
             self.graph.add_edge(pc1, pc2)
 
-    def identify_lanparties(self, size: int = 3):
+    def identify_lanparties(self, max_size: int):
         """ Stores a list of lists of inter-connected computers (i.e. every node is connected to every other node in the cluster), with a given cluster size. """
+        self.lanparties = []
+        self.matches = []
+        max_size = self.graph.number_of_nodes() if max_size is None else max_size
         for clique in nx.enumerate_all_cliques(self.graph): #i.e. every node is inter-connected
-            if len(clique) == size:
+            if 3 <= len(clique) <= max_size:
                     self.lanparties.append(clique)
-            elif len(clique) > size:
+            elif len(clique) > max_size:
                 #enumerate_all_cliques is sorted by size, so we can break here
                 break
     
     def find_lanparties_by_computername(self, starts_with: str) -> list[list[str]]:
         """ Returns a set of lanparties (list of connected nodes) which contain at least one computer/node which's name starts with a spezific character. """
-        matches = []
+        self.matches = []
         for lanparty in self.lanparties:
             for node in lanparty:
                 if node.startswith(starts_with):
-                    matches.append(lanparty)
+                    self.matches.append(lanparty)
                     break
-        return matches
 
-    def do(self):
-        self.identify_lanparties()
+    def do(self, starts_with: str = '', max_size: int = None):
+        self.identify_lanparties(max_size)
+        self.find_lanparties_by_computername(starts_with)
 
-    def checksum(self, starts_with: str = None) -> int:
-        if starts_with:
-            return sum(1 for clusters in self.find_lanparties_by_computername(starts_with))
-        else:
-            return len(self.lanparties)
+    def checksum(self) -> int:
+        return len(self.matches)
     
     def check_p2(self):
-        pass
+        top = self.matches[-1] #or self.lanparties[-1]?
+        top.sort()
+        return ','.join(top)
 
 def load_puzzle(input_file: str) -> Puzzle:
     """Load Puzzle from input file (path)."""
